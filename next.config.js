553c13e8c15123bbda1135944860ccf1af37f373
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-01-12 21:15:52 +0200

    init: set up development environment and tools

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // environment variables for use in client and server side
+  env: {
+    API_URL: "http://localhost:3000",
+    DB_LOCAL_URI: "",
+    DB_URI: "",
+  },
+  reactStrictMode: true,
+  // settings for optimized image loading
+  images: {
+    // trusted domains for image uploads
+	remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "your-website.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  } 
+}
+
+module.exports = nextConfig
