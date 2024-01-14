71993d58452b724dbc52ad603151493a3ac27313
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-01-14 19:19:17 +0200

    feat: incorporate new user-facing feature

---
+++ src/app/donate/page.tsx
@@ -0,0 +1,55 @@
+// @/app/donate/page.tsx
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Image from 'next/image';
+import qrCode from '../../../public/images/qr-pb-donate.webp';
+
+export default function DonatePage() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '20px',
+      }}
+    >
+      <Paper
+        sx={{
+          padding: '20px',
+          maxWidth: '600px',
+          textAlign: 'center',
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Важливо!!! Отримувач тільки Крутоголов Юлія Сергіївна
+        </Typography>
+        <Image
+          src={qrCode}
+          alt="QR Code"
+          width={300}
+          height={300}
+        />
+        <Typography paragraph>
+          <strong>АТ КБ Приватбанк</strong>
+        </Typography>
+        <Typography paragraph>
+          Отримувач: Крутоголов Юлія Сергіївна
+        </Typography>
+        <Typography paragraph>
+          Р/р: UA243052990000026004045030722
+        </Typography>
+        <Typography paragraph>
+          Код отримувача: 45089754
+        </Typography>
+        <Typography paragraph>
+          IBAN: UA243052990000026004045030722
+        </Typography>
+        <Typography paragraph>
+          Призначення платежу: Добровільні внески БО БФ ЯНГОЛ ЗСУ
+        </Typography>
+      </Paper>
+    </Box>
+  );
+}
