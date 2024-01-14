bd25a8a1b4fc4842d49b389e8ee902d8e36f66a7
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-01-14 18:55:49 +0200

    feat: implement new module

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,19 +1,52 @@
 // @/app/about/page.tsx
+import styles from './page.module.css';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import backPic from '../../../public/images/about-angels.jpg';
 
 export default function AboutPage() {
   return (
-    <>
-      <section>
-        <h1>Розповідаємо про Price Pulse.</h1>
-        {/*  */}
-        <p>Даний веб-застосунок має на меті показати вам інформацію про ціни на різні товари в торгівельних мережах України.</p>
-        <p>Для кожного товару фіксується найнижча ціна, яку спостерігали в певному супермаркеті під час якоїсь акції.</p>
-        <p>Таким чином, перебуваючи в магазині, ви зможете порівняти цінник обраного вами товару із цінами в застосунку.
-          Та вирішити, чи є вигідною для вас поточна ціна цього товару.</p>
-      </section>
-      <section>
-        <h2>Як користуватися Price Pulse?</h2>
-      </section>
-    </>
+    <Box
+      sx={{
+        backgroundImage: `url(${backPic.src})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        padding: '20px',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <Paper
+        sx={{
+          padding: '20px',
+          backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        }}
+      >
+        <Typography variant="h2" gutterBottom>
+          Про нас
+        </Typography>
+        <Typography paragraph>
+          <strong>Благодійний фонд &ldquo;Янгол ЗСУ&rdquo;</strong> — це ініціатива, створена з метою
+          надання підтримки воїнам Збройних Сил України.
+        </Typography>
+        <Typography paragraph>
+          Наш фонд покликаний забезпечувати допомогу воїнам-учасникам бойових дій у важкі часи.
+          Ми віримо у важливість підтримки та допомоги тим, хто віддає своє життя за захист і безпеку України.
+        </Typography>
+        <Typography paragraph>
+          <strong>Наша місія</strong> — надавати фінансову допомогу, психологічну підтримку та необхідні ресурси
+          для поліпшення роботи наших героїв. Ми віримо в те, що кожен може внести свій вклад у створення кращого
+          майбутнього для нашої країни.
+        </Typography>
+        <Typography paragraph>
+          Приєднуйтеся до нашого фонду та допоможіть нам разом підтримати тих, хто віддає своє найкраще для
+          безпеки і свободи України.
+        </Typography>
+      </Paper>
+    </Box>
   );
 }
