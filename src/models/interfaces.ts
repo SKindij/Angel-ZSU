62a5a27b8a0e17f3729900e26f111024714fc956
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-01-25 22:18:38 +0200

    chore: add new entries to the database

--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -1,1 +1,25 @@
 // @/models/interfaces.ts
+
+export interface User {
+  id:number | string;
+  name:string;
+  photo?:string;
+  professional?:string;
+}
+
+export interface TeamMember extends Omit<User, 'professional'> {
+  description?:string;
+  category:string;
+  company?:{
+    name:string;
+    logo:string;
+  };
+}
+
+
+
+
+
+
+
+
