e376459b0e12892d3ce1477dbc3cc4ac9f7deca2
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-01-25 22:19:04 +0200

    feat: implement new modules

--- src/views/Home/OurTeam.tsx
+++ src/views/Home/OurTeam.tsx
@@ -1,5 +1,111 @@
 // @/views/Home/OurTeam.tsx
 import Box from '@mui/material/Box';
+import Slider, { Settings } from 'react-slick';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import IconArrowBack from '@mui/icons-material/ArrowBack';
+import IconArrowForward from '@mui/icons-material/ArrowForward';
+// information about fund participants
+import TeamMemberCard from '@/views/Components/TeamMemberCard';
+import { MembersData } from '@/services/app-data';
+
+interface SliderArrowProps {
+  onClick?:()=>void;
+  type:'next' | 'prev';
+  className?:string;
+}
+
+const SliderArrow = ({onClick, type, className}:SliderArrowProps) => {
+  const { breakpoints } = useTheme();
+  const matchMobileView = useMediaQuery(breakpoints.down('md'));
+
+  return (
+    <IconButton
+      sx={{
+        backgroundColor: 'background.paper',
+        color: 'primary.main',
+        '&:hover': { backgroundColor: 'primary.main', color: 'primary.contrastText' },
+        bottom: '-28px !important',
+        left: 'unset !important',
+        right: type === 'prev' ? '60px !important' : '0 !important',
+        zIndex: 10,
+        boxShadow: 1,
+      }}
+      disableRipple
+      color="inherit"
+      onClick={onClick}
+      className={className}
+    >
+      {type === 'next' ? <IconArrowForward sx={{ fontSize: 22 }} /> : <IconArrowBack sx={{ fontSize: 22 }} />}
+    </IconButton>
+  );
+};
+
+const StyledDots = ({ children }) => (
+  <ul className="slick-dots">
+    {React.Children.map(children, (dot) => (
+      <li>
+        {React.cloneElement(dot as React.ReactElement, {
+          className: `slick-active${dot.props.className ? ' ' + dot.props.className : ''}`,
+        })}
+      </li>
+    ))}
+  </ul>
+);
+
+const OurTeam = () => {
+  const { breakpoints } = useTheme();
+  const matchMobileView = useMediaQuery(breakpoints.down('md'));
+
+  const sliderConfig:Settings = {
+    infinite: true,
+    speed: 300,
+    slidesToShow: matchMobileView ? 1 : 3,
+    slidesToScroll: 1,
+    prevArrow: <SliderArrow type="prev" />,
+    nextArrow: <SliderArrow type="next" />,
+    dots: true,
+    appendDots: (dots) => <StyledDots>{dots}</StyledDots>,
+    customPaging: () => (
+      <Box sx={{ height: 8, width: 30, backgroundColor: 'divider', display: 'inline-block', borderRadius: 4 }} />
+    ),
+  };
+
+  return (
+    <Box
+      id="mentors"
+      sx={{
+        pt: {
+          xs: 6,
+          md: 8,
+        },
+        pb: {
+          xs: 8,
+          md: 12,
+        },
+        backgroundColor: '#ecf3f3',
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography variant="h1" sx={{ fontSize: 40 }}>
+          Our Expert Mentors
+        </Typography>
+
+        <Slider {...sliderConfig}>
+          {data.map((item) => (
+            <MentorCardItem key={String(item.id)} item={item} />
+          ))}
+        </Slider>
+      </Container>
+    </Box>
+  );
+};
+
+export default OurTeam;
+
 
 
 
