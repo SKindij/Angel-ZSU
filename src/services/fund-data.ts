b7b38bacaf664d3372dee09c34c88eaefba161c7
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-01-26 20:35:48 +0200

    chore: remove unused files and assets

---
+++ src/services/fund-data.ts
@@ -0,0 +1,63 @@
+// @/services/fund-data.ts
+import { IOurAchievement } from '@/models/interfaces';
+
+/* ----- НАШІ З ВАМИ ДОСЯГНЕННЯ ----- */
+export const ourAchievements:IOurAchievement[] = [
+  {
+    id: 111,
+    label: 'Донарів долучилося до зборів.',
+    value: '1 000 +',
+  },
+  {
+    id: 112,
+    label: 'Зібрано з вашою допомогою з 24.02.2022.',
+    value: '300 000 +',
+  },
+  {
+    id: 113,
+    label: 'Звітів з військовослужбовцями, що дякують нам за допомогу.',
+    value: '50 +',
+  },
+];
+
+
+
+/* ----- Відео з YouTube ----- */
+const ourVideo = [
+  {
+    id: 211,
+    title: 'Обмануті волонтери і військові: як відрізнити шахраїв від чесних продавців',
+    link: 'https://www.youtube.com/watch?app=desktop&v=HNiCRffMdEE&feature=youtu.be',
+  },
+  {
+    id: 212,
+    title: '',
+    link: '',
+  },
+  {
+    id: 213,
+    title: '',
+    link: '',
+  },
+  {
+    id: 214,
+    title: '',
+    link: '',
+  },
+  {
+    id: 215,
+    title: '',
+    link: '',
+  },
+  {
+    id: 216,
+    title: '',
+    link: '',
+  },
+];
+
+/* -----  ----- */
+
+
+
+
