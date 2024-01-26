67bafce8bc5ff203f712ea79cac85fa8a9c7b090
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-01-26 20:56:26 +0200

    chore: refactor code to maintain code style consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,24 +10,36 @@ import Link from 'next/link';
 // views components
 import MainTypowriter from '@/views/Home/MainTypowriter';
 import mainPic from '../../public/images/angels.jpg';
-import OurTeam from '@/views/Home/OurTeam';
-
-
+import FundHeroTest from '@/views/Home/FundHeroTest';
+import FundCampaigns from '@/views/Home/FundCampaigns';
+import FundTeam from '@/views/Home/FundTeam';
+import FundBlog from '@/views/Home/FundBlog';
 
 export default function Home() {
   return (
     <div>
       {/* home page */}
-      <Container sx={{
-		  marginBottom:3, padding:1, textAlign:'center',
-		  color:'darkgreen', letterSpacing:3, fontSize: '1.6rem'
-      }}>
+      <Container
+        sx={{
+          marginBottom: 3,
+          padding: 1,
+          textAlign: 'center',
+          color: 'darkgreen',
+          letterSpacing: 3,
+          fontSize: '1.6rem',
+        }}
+      >
         {/* several run-on sentences */}
         <MainTypowriter />
       </Container>
+	  {/* welcome content block */}
+	  <FundHeroTest />
+	  {/* actual cash fees */}
+	  <FundCampaigns />
       {/* members of our fund */}
-      <OurTeam />
-
+	  <FundTeam />
+	  {/* some more information */}
+	  <FundBlog />
     </div>
   );
 }
--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -1,22 +1,11 @@
 // @/models/interfaces.ts
 
-export interface User {
-  id:number | string;
-  name:string;
-  photo?:string;
-  professional?:string;
-}
 
-export interface TeamMember extends Omit<User, 'professional'> {
-  description?:string;
-  category:string;
-  company?:{
-    name:string;
-    logo:string;
-  };
-}
-
-
+export interface IOurAchievement {
+  id:number;
+  label:string;
+  value:string;
+};
 
 
 
