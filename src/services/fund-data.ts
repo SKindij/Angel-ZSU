ace53b275577d628b571a293a7afe1ef12f1ac81
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-01-30 21:42:50 +0200

    chore: update information within the database

--- src/services/fund-data.ts
+++ src/services/fund-data.ts
@@ -31,8 +31,8 @@ export const ourCampaignVideos:IOurVideos[] = [
   },
   {
     id: 312,
-    title: 'Запит на авто від 203 батальйону звязку 108 бригади',
-    url: 'https://www.youtube.com/embed/mBh9iV0lTsk?si=j3ou6U1ZKbAdyp9W',
+    title: 'Запит на зимову гуму від хлопців з медичної групи підрозділу НГУ',
+    url: 'https://www.youtube.com/embed/XF1B_h_4bGs?si=bZrpmk7-4_SiYyMs',
   },
   {
     id: 313,
@@ -41,18 +41,48 @@ export const ourCampaignVideos:IOurVideos[] = [
   },
   {
     id: 314,
-    title: '',
-    url: '',
+    title: 'Запит від Білоруських добровольців, батальйону імені Дмитра Терору на засоби звязку та 3D окуляри.',
+    url: 'https://www.youtube.com/embed/i14Un9lESEk?si=2YlK_An_HZKpJIMU',
   },
   {
     id: 315,
-    title: '',
-    url: '',
+    title: 'Запит від 10 ОГШБр на 2 позашляховики для виконання бойових завдань',
+    url: 'https://www.youtube.com/embed/YLM3dQF6Ar8?si=I5KtNfjxnCOolNzQ',
   },
   {
     id: 316,
-    title: '',
-    url: '',
+    title: 'Запит від 65 Бригади Аеророзвідки на DJI Mavic 3 Fly More Combo',
+    url: 'https://www.youtube.com/embed/jkJXkKsvo7c?si=Z4Uwy8qBbNjf7qe7',
+  },
+  {
+    id: 317,
+    title: 'Запит від Взводу Ударних БПЛА 4 Мех. Батальйону Окремої Президентської Бригади ім. Б. Хмельницького',
+    url: 'https://www.youtube.com/embed/pAKI30hez10?si=KOrqtU84EhTNwTtM',
+  },
+  {
+    id: 318,
+    title: 'Запит від 35 Бригади Морської Піхоти на автомобіль',
+    url: 'https://www.youtube.com/embed/wJVzGoQC7vQ?si=pnib-vfOMlYjSTa5"',
+  },
+  {
+    id: 319,
+    title: 'Запит на авто від 203 батальйону звязку 108 бригади',
+    url: 'https://www.youtube.com/embed/mBh9iV0lTsk?si=j3ou6U1ZKbAdyp9W',
+  },
+  {
+    id: 320,
+    title: 'Запит від 3 Роти на ноши',
+    url: 'https://www.youtube.com/embed/ViZfJRB6CpA?si=dBGsO5RdT5CBd_OD',
+  },
+  {
+    id: 321,
+    title: 'Запит від Окремої Єгерської Бригади ДШВ',
+    url: 'https://www.youtube.com/embed/TBgwOZsiuvs?si=dIhXjKtts2Y2Pgpb',
+  },
+  {
+    id: 322,
+    title: 'Запит від Мобільної Вогневої Групи на прожектори',
+    url: 'https://www.youtube.com/embed/R0F0ycjZnDs?si=Of9BxysBTbLH190U',
   },
 ];
 
@@ -64,28 +94,43 @@ export const ourReportVideos:IOurVideos[] = [
   },
   {
     id: 412,
-    title: '',
-    url: '',
+    title: 'Звіт від 1 батальйону 53 ОМБр по авто',
+    url: 'https://www.youtube.com/embed/aP1f6E9OS0I?si=7xdyYFLx35GwY9uC',
   },
   {
     id: 413,
-    title: '',
-    url: '',
+    title: 'Звіт по дронах та техніці для 45 ОСБ',
+    url: 'https://www.youtube.com/embed/m5dNdkC_1F4?si=J0IZS11CUfsjXLCr',
   },
   {
     id: 414,
-    title: '',
-    url: '',
+    title: 'Звіт від спецпідрозділу ГУРа за EcoFlow',
+    url: 'https://www.youtube.com/embed/GfrIZ5Ls5Ls?si=iEkvSbYwMS0JwE0x',
   },
   {
     id: 415,
-    title: '',
-    url: '',
+    title: 'Звіт від 17 бригади 3 роти',
+    url: 'https://www.youtube.com/embed/58ezz5PRbLU?si=Y7O9pBg45kKf5Xxv',
   },
   {
     id: 416,
-    title: '',
-    url: '',
+    title: 'Звіт від 36 бригади 1 окремоного батальйону морської піхоти',
+    url: 'https://www.youtube.com/embed/WrRfq8YGi9Q?si=gfTiyddzA10NywAj',
+  },
+  {
+    id: 417,
+    title: 'Звіт від 406 ОСБ про Ecoflow',
+    url: 'https://www.youtube.com/embed/2I9P4KyS_Ic?si=RC7K9hfO5av2fbuS',
+  },
+  {
+    id: 418,
+    title: 'Звіт про допомогу на позицію Піски',
+    url: 'https://www.youtube.com/embed/8PNTfB2UGTs?si=HFhTHF-VdWsWcDsl',
+  },
+  {
+    id: 419,
+    title: 'Звіт від 71 Єгерської Бригади',
+    url: 'https://www.youtube.com/embed/_DQReyTKlOE?si=FtB7IIO73LKhua2G',
   },
 ];
 
