28359c9fe70fe8442b6a30c400b1c18e3e5da548
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-02-25 22:23:22 +0200

    init: create middleware file

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+// middleware.ts
+// provides basic functionality
+export { default } from 'next-auth/middleware';
+
+// configuring middleware to protect certain routes
+export const config = {
+  // indicates routes that require user authentication
+  matcher: ['/admin/collect/:path*', '/about']
+};
