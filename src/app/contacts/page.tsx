d806cce51e84aed42f7f380094b874e71b04481a
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-02-28 22:50:35 +0200

    feat: create new data visualization component

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -3,7 +3,6 @@ import { Metadata } from 'next';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import Image from 'next/image';
 
 import СandidateButton from '@/views/Home/teamUI/СandidateButton';
 
@@ -18,12 +17,32 @@ export default function ContactsPage() {
     <Box
       sx={{
         display: 'flex', flexDirection: 'column',
-        alignItems: 'center', padding: '20px',
+        alignItems: 'center', p: 2, mb: 4
       }}
     >
-      <Typography paragraph>
-        Тут будуть наші контакти.
+      <Typography variant="h1" gutterBottom>
+        Контактна інформація
       </Typography>
+      <Paper sx={{ p: 2, maxWidth: 400 }}>
+        <Typography variant="body1" gutterBottom>
+          <strong>Номер телефону Янгол ЗСУ:</strong> 067-955-77-44 (Telegram, Viber, WhatsApp, Signal)
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <strong>Пошта Янгол ЗСУ:</strong> cf.angel.zsu@gmail.com
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <strong>Телеграм канал:</strong> <a href="https://t.me/angel_zsu">t.me/angel_zsu</a>
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <strong>Для приватних звернень в Телеграмі:</strong> <a href="https://t.me/BF_Angel_ZSU">t.me/BF_Angel_ZSU</a>
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <strong>Офіційний Ютуб канал:</strong> <a href="https://youtube.com/@cf_angel_zsu">youtube.com/@cf_angel_zsu</a>
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <strong>Офіційний Тік Ток:</strong> <a href="https://tiktok.com/@_angel_zsu_?_t=8kBgG4kFSyb">tiktok.com/@_angel_zsu_?_t=8kBgG4kFSyb</a>
+        </Typography>
+      </Paper>
       {/* invitation to join the community */}
       <СandidateButton />
     </Box>
