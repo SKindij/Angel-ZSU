6cefc2ca521fc4be2217c7b5da9ce0cda224a6ec
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-03-26 21:54:15 +0200

    feat: introduce new feature to codebase

--- src/views/Home/FundFrame.tsx
+++ src/views/Home/FundFrame.tsx
@@ -9,25 +9,25 @@ const FundFrame:React.FC = () => {
 
   return (
     <Box sx={{ bgcolor:'background.paper', py: { xs: 1, md: 2 } }}>
-      <Container maxWidth="lg">
-        <Grid container spacing={1} sx={{ flexDirection: { xs:'column', md:'unset' } }}>
-          <Grid item xs={12} md={8}>
+      <Container>
+        <Grid container spacing={2} sx={{ flexDirection: { xs:'column', md:'unset' } }}>
+          <Grid item xs={12} md={7}>
             <iframe
-              width="100%" height="515"
+              width="100%" height="500"
               src={videoSrc} title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             />
           </Grid>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h5" component="h2">
-          Мега збір на 3 000 000 грн
+          <Grid item xs={12} md={5}>
+            <Typography variant="h2" component="h2" color="primary.main">
+              Мега збір на 3 000 000 грн
             </Typography>
             <Typography variant="body1" paragraph>
-            Долучайтесь до збору та отримайте змогу виграти один з сотні ексклюзивних подарунків!
+              Долучайтесь до збору та отримайте змогу виграти один з сотні ексклюзивних подарунків!
             </Typography>
             <Typography variant="body1" paragraph>
-            Головний ЛОТ збору - прапор з підписом Валерія ЗАЛУЖНОГО!
+              Головний ЛОТ збору - прапор з підписом Валерія ЗАЛУЖНОГО!
             </Typography>
           </Grid>
         </Grid>
