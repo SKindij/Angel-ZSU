075d2aaebeacd7f9e8e0151d592a3554d88fa1ea
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-03-26 21:03:04 +0200

    style: standardize CSS styles for cohesive design

--- src/views/Home/FundFrame.tsx
+++ src/views/Home/FundFrame.tsx
@@ -1,5 +1,6 @@
 // @/views/Home/FundFrame.tsx
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
@@ -8,31 +9,31 @@ const FundFrame:React.FC = () => {
 
   return (
     <Box sx={{ bgcolor:'background.paper', py: { xs: 1, md: 2 } }}>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <iframe
-            width="100%" height="515"
-            src={videoSrc} title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </Grid>
-        <Grid item xs={4}>
-          <Typography variant="h5" component="h2">
-          Мега збір на 3 000 000грн
-          </Typography>
-          <Typography variant="body1">
+      <Container maxWidth="lg">
+        <Grid container spacing={1} sx={{ flexDirection: { xs:'column', md:'unset' } }}>
+          <Grid item xs={12} md={8}>
+            <iframe
+              width="100%" height="515"
+              src={videoSrc} title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h5" component="h2">
+          Мега збір на 3 000 000 грн
+            </Typography>
+            <Typography variant="body1" paragraph>
             Долучайтесь до збору та отримайте змогу виграти один з сотні ексклюзивних подарунків!
-          </Typography>
-          <Typography variant="body1">
+            </Typography>
+            <Typography variant="body1" paragraph>
             Головний ЛОТ збору - прапор з підписом Валерія ЗАЛУЖНОГО!
-          </Typography>
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
+      </Container>
     </Box>
   );
 };
 
 export default FundFrame;
-
-
